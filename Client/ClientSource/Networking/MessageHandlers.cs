084825b833442ebcd49001e52f8dd7c3b19fec3e
Deniss S <dsuharevskis@gmail.com>
2024-06-26 01:37:25 +0100

    unececarry updates

--- Client/ClientSource/Networking/MessageHandlers.cs
+++ Client/ClientSource/Networking/MessageHandlers.cs
@@ -70,6 +70,7 @@ public static partial class NetworkingManager
 	[MessageHandler((ushort) NetworkMessageID.EndTurn)]
 	private static void RecieveEndTrugn(Message message)
 	{
+		Log.Message("NETWORKING","end turn recieved");
 		GameManager.SetEndTurn();
 	}
 	[MessageHandler((ushort) NetworkMessageID.CaptureNotif)]
@@ -84,6 +85,7 @@ public static partial class NetworkingManager
 	[MessageHandler((ushort)NetworkMessageID.GameData)]
 	private static void ReciveGameUpdate(Message message)
 	{
+		Log.Message("NETWORKING","recived game data");
 		GameManager.GameStateData data = message.GetSerializable<GameManager.GameStateData>();
 		GameManager.SetData(data);
 	}
--- SharedSource/ReplaySequence/SequenceManager.cs
+++ SharedSource/ReplaySequence/SequenceManager.cs
@@ -218,7 +218,7 @@ public class SequenceManager
 					NetworkingManager.SendSequenceExecuted();
 			
 #else
-			NetworkingManager.SendGameData();
+			
 			GameManager.ShouldRecalculateUnitPositions = true;
 #endif
 			hasSequece = false;
